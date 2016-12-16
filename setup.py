a5f029f133ef251f4ed53ad11bdcdfefcc4fb446
Sapan Bhatia <gwsapan@gmail.com>
2016-12-16 18:56:12 +0100

    Working version.

---
+++ setup.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='makefile-style-checker',
+      version='1.0',
+      description='Check style of Makefile',
+      author='Sapan Bhatia',
+      author_email='sapan@onlab.us',
+      url='https://github.com/sb98052/makefile-style-checker',
+      packages=['pymake'],
+     )
+
+
